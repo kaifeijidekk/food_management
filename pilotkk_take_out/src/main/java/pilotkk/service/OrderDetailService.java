2e76daa4172877470645afe4b270ea1a1af9210e
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/OrderDetailService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/OrderDetailService.java
@@ -0,0 +1,8 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.OrderDetail;
+
+public interface OrderDetailService extends IService<OrderDetail> {
+
+}
