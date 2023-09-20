64843788de9638fa3327f84c4035796433897f91
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/mapper/OrderDetailMapper.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/mapper/OrderDetailMapper.java
@@ -0,0 +1,10 @@
+package pilotkk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import pilotkk.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
