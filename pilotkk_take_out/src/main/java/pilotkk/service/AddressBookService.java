45692ceaf65a9cbd1fa3e2f1e15bf1d2e826a798
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/AddressBookService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/AddressBookService.java
@@ -0,0 +1,8 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+
+}
