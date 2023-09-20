a35c0ca0888f58bcd027c0be7e0ac441a4deec04
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/mapper/ShoppingCartMapper.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/mapper/ShoppingCartMapper.java
@@ -0,0 +1,10 @@
+package pilotkk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import pilotkk.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ShoppingCartMapper extends BaseMapper<ShoppingCart> {
+
+}
