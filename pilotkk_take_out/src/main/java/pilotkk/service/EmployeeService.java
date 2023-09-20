147348c563ba0f995c97046fffdf71180f5760d1
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/EmployeeService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
