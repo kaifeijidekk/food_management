2a2d8de308adf1adea22263b9d25769d0d56c17f
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/SetmealDishService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/SetmealDishService.java
@@ -0,0 +1,7 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.SetmealDish;
+
+public interface SetmealDishService extends IService<SetmealDish> {
+}
