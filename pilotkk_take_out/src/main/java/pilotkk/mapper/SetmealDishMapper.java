017f6dd77cef81f9d971837b79417c120981f7a1
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/mapper/SetmealDishMapper.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/mapper/SetmealDishMapper.java
@@ -0,0 +1,9 @@
+package pilotkk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import pilotkk.entity.SetmealDish;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+}
