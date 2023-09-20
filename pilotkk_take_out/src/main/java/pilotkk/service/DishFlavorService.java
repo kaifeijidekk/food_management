60a16c817c5c780eb89dfc4615ffddeb7f91c1bc
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/DishFlavorService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/DishFlavorService.java
@@ -0,0 +1,7 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.DishFlavor;
+
+public interface DishFlavorService extends IService<DishFlavor> {
+}
