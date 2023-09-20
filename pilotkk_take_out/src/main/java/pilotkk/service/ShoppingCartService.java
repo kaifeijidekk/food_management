62cb00fdc72187c01ef662e8087af73225b52854
kaifeijidekk <yanyuchennn@gmail.com>
2023-09-20 16:25:40 -0400

    Updated ./pilotkk_take_out/src/main/java/pilotkk/service/ShoppingCartService.java

---
+++ pilotkk_take_out/src/main/java/pilotkk/service/ShoppingCartService.java
@@ -0,0 +1,8 @@
+package pilotkk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import pilotkk.entity.ShoppingCart;
+
+public interface ShoppingCartService extends IService<ShoppingCart> {
+
+}
